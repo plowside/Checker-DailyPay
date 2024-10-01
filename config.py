08c66af9127a73916891e2a42e46371340d2be6b
plowside <plowsidee@gmail.com>
2024-10-02 02:32:27 +0700

    fixed

--- config.py
+++ config.py
@@ -1,8 +1,9 @@
 ### MAIN
-max_concurrent_tasks = 50 # Threads
+max_concurrent_tasks = 100 # Threads
 accounts_path = 'data/accounts.txt' # Accounts
 custom_path = 'data/custom.txt' # Custom
 hits_save_path = 'data/hits.txt' # Hits
+hits_without_2fa_path = 'data/hits_2fa' # Hits logged in without 2fa
 
 ### PROXY
 proxy_path = 'data/proxy.txt' # Файл с прокси
