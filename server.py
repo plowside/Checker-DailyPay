937763a6981dd1dff59cdaa51521540b8c75adab
plowside <plowsidee@gmail.com>
2024-10-02 16:21:12 +0700

    Update server.py

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-import asyncio, os
+import asyncio, playwright, os
 import random
 from fastapi import FastAPI, Response
 from contextlib import asynccontextmanager
@@ -15,6 +15,7 @@ class FSM:
         self.browser = None
         self.context = None
         self.pages = []
+        self.context_pool = []
 
 storage = FSM()
 storage.user_agents = open('assets/useragets.txt', 'r', encoding='utf-8').read().split('\n')
@@ -43,7 +44,7 @@ TIMEZONES = [
     'Europe/Berlin',
 ]
 
-def randomize_browser_settings():
+async def randomize_browser_settings():
     user_agent = random.choice(USER_AGENTS)
     viewport = random.choice(VIEWPORTS)
     language = random.choice(LANGUAGES)
@@ -55,46 +56,84 @@ async def launch_browser():
     while True:
         try:
             async with async_playwright() as p:
-                launch_options = {'headless': True, 'slow_mo': random.randint(50, 150), 'args': [
-                    "--disable-blink-features=AutomationControlled",
-                    "--no-sandbox",
-                    "--disable-extensions",
-                    "--disable-gpu",
-                    "--disable-dev-shm-usage"
-                ], 'proxy': {
-                    'server': 'residential.digiproxy.cc:5959',
-                    'username': 'KoCLLHFAo2MMXER-res-us',
-                    'password': 'S9aTfmMlcaxESWo'
-                }}
-
-                browser = await p.chromium.launch(**launch_options)
-                storage.pages = []
-                storage.browser = browser
-                await asyncio.sleep(99999)
+                if storage.browser is not None:
+                    try: await storage.browser.close()
+                    except: ...
+                launch_options = {
+                    'headless': True,
+                    'args': [
+                        "--disable-blink-features=AutomationControlled",
+                        "--no-sandbox",
+                        "--disable-extensions",
+                        "--disable-gpu",
+                        "--disable-dev-shm-usage"
+                    ],
+                    'proxy': {
+                        'server': 'residential.digiproxy.cc:5959',
+                        'username': 'KoCLLHFAo2MMXER-res-us',
+                        'password': 'S9aTfmMlcaxESWo'
+                    }
+                }
+                storage.browser = await p.chromium.launch(**launch_options)
+                print('Browser inited')
+                await asyncio.sleep(3600)
         except Exception as e:
-            print('browser', e)
+            print('browser error:', e)
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     asyncio.get_event_loop().create_task(launch_browser())
+    asyncio.get_event_loop().create_task(context_pool_filler())
     yield
     await storage.browser.close()
     print('Browser closed')
 
 app = FastAPI(lifespan=lifespan)
 
-@app.get("/token")
-async def get_token():
+async def context_pool_filler():
+    pool_size = 100
+    last_pool_size = 0
+    while True:
+        if last_pool_size == len(storage.context_pool) and len(storage.context_pool) == pool_size:
+            await asyncio.sleep(.5)
+            continue
+        print('pool_size', len(storage.context_pool))
+        while len(storage.context_pool) < pool_size:
+            tasks = [create_context_and_page(True) for x in range(pool_size - len(storage.context_pool))]
+            while storage.browser is None:
+                await asyncio.sleep(.5)
+                continue
+            print('filling_pool', len(tasks))
+            res = await asyncio.gather(*tasks)
+        last_pool_size = len(storage.context_pool)
+
+async def create_context_and_page(add_to_pool: True):
+    user_agent, viewport, language, timezone = await randomize_browser_settings()
+    context = await storage.browser.new_context(
+        user_agent=user_agent,
+        viewport={"width": viewport[0], "height": viewport[1]},
+        locale=language,
+        timezone_id=timezone
+    )
+    page = await context.new_page()
     try:
-        user_agent, viewport, language, timezone = randomize_browser_settings()
-        context = await storage.browser.new_context(
-            user_agent=user_agent,
-            viewport={"width": viewport[0], "height": viewport[1]},
-            locale=language,
-            timezone_id=timezone
-        )
-        page = await context.new_page()
         await page.goto(index_file_path)
+    except playwright._impl._errors.TimeoutError:
+        asyncio.create_task(close_context_and_page(context, page))
+        return
+    except Exception as e:
+        print('Error on page.goto', type(e), '|', e)
+        asyncio.create_task(close_context_and_page(context, page))
+        return
+    if add_to_pool: storage.context_pool.append([context, page, user_agent])
+    return context, page, user_agent
+
+async def get_token():
+    while True:
+        if len(storage.context_pool) == 0:
+            context, page, user_agent = await create_context_and_page(False)
+        else:
+            context, page, user_agent = storage.context_pool.pop(0)
         token = await page.evaluate('''() => {
             return new Promise((resolve) => {
                 _castle('createRequestToken').then(requestToken => {
@@ -104,7 +143,20 @@ async def get_token():
                 });
             });
         }''')
+        asyncio.create_task(close_context_and_page(context, page))
+        return token, user_agent
+
+async def close_context_and_page(context, page):
+    try:
         await page.close()
+        await context.close()
+    except Exception as e:
+        print(f"Error closing page or context: {e}")
+
+@app.get("/token")
+async def route_token():
+    try:
+        token, user_agent = await get_token()
         return {'status': True, 'token': token, 'user-agent': user_agent}
     except Exception as e:
         return {'status': False, 'error': str(e)}
@@ -114,6 +166,5 @@ async def get_html_page():
     html_content = open(index_file_path, 'r', encoding='utf-8').read()
     return Response(content=html_content, media_type="text/html")
 
-# Main entry point for server
 if __name__ == '__main__':
     uvicorn.run("server:app", host="0.0.0.0", port=8000, log_level="info")
